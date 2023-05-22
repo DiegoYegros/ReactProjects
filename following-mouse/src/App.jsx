de0cc501e57052c53ef5721af74ed7061f518acb
Diego <diegoyegrosa@gmail.com>
2023-05-22 09:27:28 -0400

    first commit

---
+++ following-mouse/src/App.jsx
@@ -0,0 +1,40 @@
+import { useState, useEffect } from "react"
+function App() {
+  const [enabled, setEnabled] = useState(false)
+  const [position, setPosition] = useState({ x: 0, y: 0 })
+  useEffect(() => {
+    const handleMove = (event) => {
+      const { clientX, clientY } = event
+      setPosition({ x: clientX, y: clientY })
+    }
+
+    if (enabled) {
+      window.addEventListener('pointermove', handleMove)
+    }
+    return () => {
+      window.removeEventListener('pointermove', handleMove)
+    }
+  }, [enabled])
+
+  return (
+    <main>
+      <div style={{
+        position: 'absolute',
+        backgroundColor: '#09f',
+        borderRadius: '50%',
+        opacity: `${enabled ? 0.8 : 0}`,
+        pointerEvents: 'none',
+        left: -20,
+        top: -20,
+        width: 40,
+        height: 40,
+        transform: `translate(${position.x}px, ${position.y}px)`
+
+      }} />
+      <h3>Proyecto 3</h3>
+      <button onClick={() => setEnabled(!enabled)}>{enabled ? 'Desactivar' : 'Activar'} puntero</button>
+    </main>
+  )
+}
+
+export default App
